b18db6b46e41243909ff284bdf031cce04883595
ru59tew <annika.kilian@campus.lmu.de>
2020-07-11 11:24:46 +0200

    deleted recordings manager (b/c only needed for reading data from csvs) and the corresponding values in constants.py

--- src/storage/Constants.py
+++ src/storage/Constants.py
@@ -9,9 +9,6 @@ numberOfChannels     = 8
 windowSize           = 0.15 #in s -> 150ms = 0.15s -> our sampling rate = 500Hz-> one sample every 0.002s
 samplesPerWindow     = int(windowSize / (1. / samplingRate))
 windowShift          = int(samplesPerWindow / 2)
-effect               = ' thumb vibrato up-down'
-fileNameOfRecording  = "test_rda_full"
-recordingsPath       = "./recording_thursday/brainvision_recorder/" + fileNameOfRecording
 lowestValidChannel   = 0
 highestValidChannel  = 7
 #for study
