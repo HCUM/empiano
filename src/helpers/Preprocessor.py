cfabb3dd1267da78bf9c23acbe5064cca27270fb
ru59tew <annika.kilian@campus.lmu.de>
2020-07-20 15:56:53 +0200

    deleted unused methods in preprocessor

--- src/helpers/Preprocessor.py
+++ src/helpers/Preprocessor.py
@@ -1,19 +1,6 @@
 from scipy.signal import butter, lfilter
 import storage.Constants as constants
 
-
-def bandpassAllChannels(eegDf, lowcutParam=constants.lowerBoundCutOutFreq,
-                        highcutParam=constants.upperBoundCutOutFreq, fsParam=constants.samplingRate):
-    for i in range(constants.numberOfChannels):
-        eegDf[i] = butter_band_filter(eegDf[i], lowcutParam, highcutParam, fsParam, 'band')
-
-
-def bandStopAllChannels(eegDf, lowParam=constants.lowBandStopFreq,
-                        highParam=constants.highBandStopFreq, fsParam=constants.samplingRate):
-    for i in range(constants.numberOfChannels):
-        eegDf[i] = butter_band_filter(eegDf[i], lowParam, highParam, fsParam, 'bandstop')
-
-
 def butter_band(lowcut_param, highcut_param, fs_param, type, order=5):
     nyq  = 0.5 * fs_param
     low  = lowcut_param / nyq
