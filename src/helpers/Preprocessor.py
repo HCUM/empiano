7105157c1bcf10b39991973f4f92c1c092728e82
ru59tew <annika.kilian@campus.lmu.de>
2020-08-18 18:38:53 +0200

    in theory the sos filters from embody cshould work haha

--- src/helpers/Preprocessor.py
+++ src/helpers/Preprocessor.py
@@ -1,5 +1,18 @@
 from scipy.signal import butter, lfilter
+import scipy.signal as signal
 import storage.Constants as constants
+
+
+def butter_bandstop_filter(data, cutoff_low, cutoff_high, nyq_freq, order=3):
+    sos = signal.butter(order, [cutoff_low / nyq_freq, cutoff_high / nyq_freq], btype='bandstop', output='sos')
+    y = signal.sosfiltfilt(sos, data)
+    return y
+
+def butter_bandpass_filter(data, cutoff_low, cutoff_high, nyq_freq, order=3):
+    sos = signal.butter(order, [cutoff_low / nyq_freq, cutoff_high / nyq_freq], btype='bandpass', output='sos')
+    y = signal.sosfiltfilt(sos, data)
+    return y
+
 
 def butter_band(lowcut_param, highcut_param, fs_param, type, order=5):
     nyq  = 0.5 * fs_param
@@ -19,11 +32,16 @@ def performPreprocessing(rawData):
     bandStopData = []
     for channel in rawData:
         # apply filters
-        bandChannelData = butter_band_filter(channel, constants.lowerBoundCutOutFreq,
-                               constants.upperBoundCutOutFreq, constants.samplingRate, 'band')
+        bandChannelData = butter_bandpass_filter(channel, constants.lowerBoundCutOutFreq,
+                               constants.upperBoundCutOutFreq, constants.samplingRate * 0.5)
 
-        bandStopData.append(
-            butter_band_filter(bandChannelData, constants.lowBandStopFreq,
-                               constants.highBandStopFreq, constants.samplingRate, 'bandstop'))
+        #butter_band_filter(channel, constants.lowerBoundCutOutFreq,
+        #                       constants.upperBoundCutOutFreq, constants.samplingRate, 'band')
+
+        bandStopData.append(butter_bandstop_filter(bandChannelData, constants.lowBandStopFreq,
+                               constants.highBandStopFreq, constants.samplingRate * 0.5))
+
+#            butter_band_filter(bandChannelData, constants.lowBandStopFreq,
+#                               constants.highBandStopFreq, constants.samplingRate, 'bandstop'))
 
     return bandStopData
