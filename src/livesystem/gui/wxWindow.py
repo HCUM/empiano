f63688ea4f5674c9f2a2f24035b219023f012602
ru59tew <annika.kilian@campus.lmu.de>
2020-08-20 16:21:00 +0200

    view Verbesserungen

--- src/livesystem/gui/wxWindow.py
+++ src/livesystem/gui/wxWindow.py
@@ -14,8 +14,8 @@ from wx.lib.intctrl import IntCtrl
 
 class MyFrame(wx.Frame):
     def __init__(self, controller):
-        wx.Frame.__init__(self, None, wx.ID_ANY, title= u"EMPiano",  size = wx.Size( 1000,520 ))
-        self.SetSizeHints(wx.Size(1000, 520), wx.DefaultSize)
+        wx.Frame.__init__(self, None, wx.ID_ANY, title= u"EMPiano",  size = wx.Size( 1000,600 ))
+        self.SetSizeHints(wx.Size(1000, 600), wx.DefaultSize)
 
         self.controller = controller
 
@@ -102,173 +102,183 @@ class StartPanel ( wx.Panel ):
 ###########################################################################
 
 class SettingsPanel ( wx.Panel ):
-    def __init__( self, parent, controller, id = wx.ID_ANY, pos = wx.DefaultPosition,
-                  size = wx.Size( 430,500 ), style = wx.TAB_TRAVERSAL, name = wx.EmptyString ):
-        wx.Panel.__init__ ( self, parent, id = id, pos = pos, size = size, style = style, name = name )
+    def __init__(self, parent, controller, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.Size(430, 500), style=wx.TAB_TRAVERSAL,
+                 name=wx.EmptyString):
+        wx.Panel.__init__(self, parent, id=id, pos=pos, size=size, style=style, name=name)
 
         self.controller = controller
 
-        verticalBoxes = wx.BoxSizer( wx.VERTICAL )
+        verticalBoxes = wx.BoxSizer(wx.VERTICAL)
 
-        self.dataAcquisitionLabel = wx.StaticText( self, wx.ID_ANY, u"EMG - Data Acquisition", wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.dataAcquisitionLabel.Wrap( -1 )
+        verticalBoxes.Add((0, 0), 1, wx.EXPAND, 5)
 
-        self.dataAcquisitionLabel.SetFont( wx.Font( 13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Lucida Grande" ) )
-        self.dataAcquisitionLabel.SetForegroundColour( wx.Colour( 17, 133, 49 ) )
+        self.dataAcquisitionLabel = wx.StaticText(self, wx.ID_ANY, u"EMG - Data Acquisition", wx.DefaultPosition,
+                                                  wx.DefaultSize, 0)
+        self.dataAcquisitionLabel.Wrap(-1)
+        self.dataAcquisitionLabel.SetFont(
+            wx.Font(13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Lucida Grande"))
+        self.dataAcquisitionLabel.SetForegroundColour(wx.Colour(17, 133, 49))
+        verticalBoxes.Add(self.dataAcquisitionLabel, 0, wx.EXPAND | wx.ALL, 5)
 
-        verticalBoxes.Add( self.dataAcquisitionLabel, 0, wx.EXPAND|wx.ALL, 5 )
+        flexGridDataAcquisition = wx.FlexGridSizer(0, 2, 0, 57)
+        flexGridDataAcquisition.AddGrowableCol(0)
+        flexGridDataAcquisition.SetFlexibleDirection(wx.BOTH)
+        flexGridDataAcquisition.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
-        flexGridDataAcquisition = wx.FlexGridSizer( 0, 2, 0, 57 )
-        flexGridDataAcquisition.AddGrowableCol( 0 )
-        flexGridDataAcquisition.AddGrowableCol( 1 )
-        flexGridDataAcquisition.SetFlexibleDirection( wx.BOTH )
-        flexGridDataAcquisition.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-
-        self.amtChannelsLabel = wx.StaticText(self, wx.ID_ANY, u"Amount of Electrodes/Channels: ",
-                                              wx.DefaultPosition, wx.DefaultSize, 0)
+        self.amtChannelsLabel = wx.StaticText(self, wx.ID_ANY, u"Amount of Electrodes/Channels: ", wx.DefaultPosition,
+                                              wx.DefaultSize, 0)
         self.amtChannelsLabel.Wrap(-1)
-
         flexGridDataAcquisition.Add(self.amtChannelsLabel, 0, wx.ALL | wx.EXPAND, 5)
 
-        self.amtElectrodesInput = IntCtrl(self, wx.ID_ANY, 8, wx.DefaultPosition,
-                                             wx.DefaultSize, wx.TE_RIGHT, min=1)
-
+        self.amtElectrodesInput = wx.TextCtrl(self, wx.ID_ANY, u"8", wx.DefaultPosition, wx.DefaultSize, wx.TE_RIGHT)
         flexGridDataAcquisition.Add(self.amtElectrodesInput, 0, wx.ALL, 5)
 
+        verticalBoxes.Add(flexGridDataAcquisition, 0, wx.ALL | wx.EXPAND, 5)
 
-        verticalBoxes.Add( flexGridDataAcquisition, 0, wx.ALL|wx.EXPAND, 5 )
+        self.preprocessingLabel = wx.StaticText(self, wx.ID_ANY, u"Preprocessing - Filters", wx.DefaultPosition,
+                                                wx.DefaultSize, 0)
+        self.preprocessingLabel.Wrap(-1)
 
-        self.preprocessingLabel = wx.StaticText( self, wx.ID_ANY, u"Preprocessing - Filters",
-                                                 wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.preprocessingLabel.Wrap( -1 )
+        self.preprocessingLabel.SetFont(
+            wx.Font(13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Lucida Grande"))
+        self.preprocessingLabel.SetForegroundColour(wx.Colour(17, 133, 49))
 
-        self.preprocessingLabel.SetFont( wx.Font( 13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
-                                                  wx.FONTWEIGHT_NORMAL, False, "Lucida Grande" ) )
-        self.preprocessingLabel.SetForegroundColour( wx.Colour( 17, 133, 49 ) )
+        verticalBoxes.Add(self.preprocessingLabel, 0, wx.ALL, 5)
 
-        verticalBoxes.Add( self.preprocessingLabel, 0, wx.ALL, 5 )
+        flexGridPreprocessing = wx.FlexGridSizer(0, 2, 0, 0)
+        flexGridPreprocessing.AddGrowableCol(0)
+        flexGridPreprocessing.SetFlexibleDirection(wx.BOTH)
+        flexGridPreprocessing.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
-        flexGridPreprocessing = wx.FlexGridSizer( 0, 2, 0, 0 )
-        flexGridPreprocessing.AddGrowableCol( 0 )
-        flexGridPreprocessing.AddGrowableCol( 1 )
-        flexGridPreprocessing.SetFlexibleDirection( wx.BOTH )
-        flexGridPreprocessing.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-
-        self.lowCutoffBandpassLabel = wx.StaticText(self, wx.ID_ANY, u"Low Cutoff Frequency for Bandpass Filter:", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.lowCutoffBandpassLabel = wx.StaticText(self, wx.ID_ANY, u"Low Cutoff Frequency for Bandpass Filter:",
+                                                    wx.DefaultPosition, wx.DefaultSize, 0)
         self.lowCutoffBandpassLabel.Wrap(-1)
 
         flexGridPreprocessing.Add(self.lowCutoffBandpassLabel, 0, wx.ALL | wx.EXPAND, 5)
 
-        self.lowBandpassInput = wx.TextCtrl(self, wx.ID_ANY, u"2.0", wx.DefaultPosition, wx.DefaultSize, wx.TE_READONLY | wx.TE_RIGHT)
+        self.lowBandpassInput = wx.TextCtrl(self, wx.ID_ANY, u"2.0", wx.DefaultPosition, wx.DefaultSize,
+                                            wx.TE_READONLY | wx.TE_RIGHT)
         flexGridPreprocessing.Add(self.lowBandpassInput, 0, wx.ALL | wx.LEFT, 5)
 
-        self.highCutoffBandpassLabel = wx.StaticText(self, wx.ID_ANY, u"High Cutoff Frequency for Bandpass Filter:\n", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.highCutoffBandpassLabel = wx.StaticText(self, wx.ID_ANY,
+                                                        u"High Cutoff Frequency for Bandpass Filter:\n",
+                                                     wx.DefaultPosition, wx.DefaultSize, 0)
         self.highCutoffBandpassLabel.Wrap(-1)
 
         flexGridPreprocessing.Add(self.highCutoffBandpassLabel, 0, wx.ALL | wx.EXPAND, 5)
 
-        self.highBandpassInput = wx.TextCtrl(self, wx.ID_ANY, u"100.0", wx.DefaultPosition, wx.DefaultSize, wx.TE_READONLY | wx.TE_RIGHT)
+        self.highBandpassInput = wx.TextCtrl(self, wx.ID_ANY, u"100.0", wx.DefaultPosition, wx.DefaultSize,
+                                             wx.TE_READONLY | wx.TE_RIGHT)
         flexGridPreprocessing.Add(self.highBandpassInput, 0, wx.ALL, 5)
 
-        self.lowCutoffBandstopLabel = wx.StaticText(self, wx.ID_ANY, u"Low Cutoff Frequency for Bandstop Filter:", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.lowCutoffBandstopLabel = wx.StaticText(self, wx.ID_ANY, u"Low Cutoff Frequency for Bandstop Filter:",
+                                                    wx.DefaultPosition, wx.DefaultSize, 0)
         self.lowCutoffBandstopLabel.Wrap(-1)
 
         flexGridPreprocessing.Add(self.lowCutoffBandstopLabel, 0, wx.ALL | wx.EXPAND, 5)
 
-        self.lowBandstopInput = wx.TextCtrl(self, wx.ID_ANY, u"49.0", wx.DefaultPosition, wx.DefaultSize, wx.TE_READONLY | wx.TE_RIGHT)
+        self.lowBandstopInput = wx.TextCtrl(self, wx.ID_ANY, u"49.0", wx.DefaultPosition, wx.DefaultSize,
+                                            wx.TE_READONLY | wx.TE_RIGHT)
         flexGridPreprocessing.Add(self.lowBandstopInput, 0, wx.ALL, 5)
 
-        self.highCutoffBandstopLabel = wx.StaticText(self, wx.ID_ANY, u"High Cutoff Frequency for Bandstop Filter:", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.highCutoffBandstopLabel = wx.StaticText(self, wx.ID_ANY, u"High Cutoff Frequency for Bandstop Filter:",
+                                                     wx.DefaultPosition, wx.DefaultSize, 0)
         self.highCutoffBandstopLabel.Wrap(-1)
 
         flexGridPreprocessing.Add(self.highCutoffBandstopLabel, 0, wx.ALL | wx.EXPAND, 5)
 
-        self.highBandstopInput = wx.TextCtrl(self, wx.ID_ANY, u"51.0", wx.DefaultPosition, wx.DefaultSize, wx.TE_READONLY | wx.TE_RIGHT)
-        flexGridPreprocessing.Add(self.highBandstopInput, 0, wx.ALL, 5)
+        self.high_bandstop_input = wx.TextCtrl(self, wx.ID_ANY, u"51.0", wx.DefaultPosition, wx.DefaultSize,
+                                               wx.TE_READONLY | wx.TE_RIGHT)
+        flexGridPreprocessing.Add(self.high_bandstop_input, 0, wx.ALL, 5)
+
+        verticalBoxes.Add(flexGridPreprocessing, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.svmSettingsLabel = wx.StaticText(self, wx.ID_ANY, u"SVM - Settings", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.svmSettingsLabel.Wrap(-1)
+
+        self.svmSettingsLabel.SetFont(
+            wx.Font(13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Lucida Grande"))
+        self.svmSettingsLabel.SetForegroundColour(wx.Colour(17, 133, 49))
+
+        verticalBoxes.Add(self.svmSettingsLabel, 0, wx.ALL, 5)
+
+        flexGridSVMSettings = wx.FlexGridSizer(2, 2, 0, 0)
+        flexGridSVMSettings.AddGrowableCol(0)
+        flexGridSVMSettings.SetFlexibleDirection(wx.BOTH)
+        flexGridSVMSettings.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
+
+        verticalBoxSizerSVMLeft = wx.BoxSizer(wx.VERTICAL)
+
+        self.windowSizeLabel = wx.StaticText(self, wx.ID_ANY, u"Size for Sliding Window (in s):", wx.DefaultPosition,
+                                             wx.DefaultSize, 0)
+        self.windowSizeLabel.Wrap(-1)
+        verticalBoxSizerSVMLeft.Add(self.windowSizeLabel, 0, wx.ALL | wx.EXPAND, 5)
+
+        self.dataValCorrectionLabel = wx.StaticText(self, wx.ID_ANY, u"Value for Correcting the Data:",
+                                                    wx.DefaultPosition, wx.DefaultSize, 0)
+        self.dataValCorrectionLabel.Wrap(-1)
+        verticalBoxSizerSVMLeft.Add(self.dataValCorrectionLabel, 0, wx.ALL | wx.EXPAND, 5)
 
 
-        verticalBoxes.Add( flexGridPreprocessing, 0, wx.ALL|wx.EXPAND, 5 )
+        flexGridSVMSettings.Add(verticalBoxSizerSVMLeft, 1, wx.EXPAND, 5)
 
-        self.svmSettingsLabel = wx.StaticText( self, wx.ID_ANY, u"SVM - Settings", wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.svmSettingsLabel.Wrap( -1 )
+        verticalBoxSizerSVMRight = wx.BoxSizer(wx.VERTICAL)
 
-        self.svmSettingsLabel.SetFont( wx.Font( 13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Lucida Grande" ) )
-        self.svmSettingsLabel.SetForegroundColour( wx.Colour( 17, 133, 49 ) )
+        self.windowSizeInput = wx.TextCtrl(self, wx.ID_ANY, u"0.15", wx.DefaultPosition, wx.DefaultSize,
+                                           wx.TE_READONLY | wx.TE_RIGHT)
+        verticalBoxSizerSVMRight.Add(self.windowSizeInput, 0, wx.ALL, 5)
 
-        verticalBoxes.Add( self.svmSettingsLabel, 0, wx.ALL, 5 )
+        self.dataValCorrectionInput = wx.TextCtrl(self, wx.ID_ANY, u"0.000001", wx.DefaultPosition, wx.DefaultSize,
+                                                  wx.TE_READONLY | wx.TE_RIGHT)
+        verticalBoxSizerSVMRight.Add(self.dataValCorrectionInput, 0, wx.ALL, 5)
+        flexGridSVMSettings.Add(verticalBoxSizerSVMRight, 1, wx.EXPAND, 5)
 
-        flexGridSVMSettings = wx.FlexGridSizer( 2, 2, 0, 78 )
-        flexGridSVMSettings.AddGrowableCol( 0 )
-        flexGridSVMSettings.AddGrowableCol( 1 )
-        flexGridSVMSettings.SetFlexibleDirection( wx.BOTH )
-        flexGridSVMSettings.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+        verticalBoxes.Add(flexGridSVMSettings, 0, wx.ALL | wx.EXPAND, 5)
 
-        self.windowSizeLabel = wx.StaticText( self, wx.ID_ANY, u"Size for Sliding Window (in s):", wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.windowSizeLabel.Wrap( -1 )
-
-        flexGridSVMSettings.Add( self.windowSizeLabel, 0, wx.ALL|wx.EXPAND, 5 )
-
-        self.windowSizeInput = wx.TextCtrl(self, wx.ID_ANY, u"0.15", wx.DefaultPosition, wx.DefaultSize, wx.TE_READONLY | wx.TE_RIGHT)
-        flexGridSVMSettings.Add(self.windowSizeInput, 0, wx.ALL, 5)
-
-        self.dataValCorrectionLabel = wx.StaticText( self, wx.ID_ANY, u"Value for Correcting the Data:", wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.dataValCorrectionLabel.Wrap( -1 )
-
-        flexGridSVMSettings.Add( self.dataValCorrectionLabel, 0, wx.ALL|wx.EXPAND, 5 )
-
-        self.dataValCorrectionInput = wx.TextCtrl( self, wx.ID_ANY, u"0.000001", wx.DefaultPosition, wx.DefaultSize, wx.TE_READONLY|wx.TE_RIGHT )
-        flexGridSVMSettings.Add( self.dataValCorrectionInput, 0, wx.ALL, 5 )
-
-
-        verticalBoxes.Add( flexGridSVMSettings, 0, wx.ALL|wx.EXPAND, 5 )
-
-        self.midiSettingsLabel = wx.StaticText( self, wx.ID_ANY, u"MIDI - Settings", wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.midiSettingsLabel.Wrap( -1 )
-
-        self.midiSettingsLabel.SetFont( wx.Font( 13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Lucida Grande" ) )
-        self.midiSettingsLabel.SetForegroundColour( wx.Colour( 17, 133, 49 ) )
-
-        verticalBoxes.Add( self.midiSettingsLabel, 0, wx.ALL, 5 )
+        #MIDI Settings
+        self.midiSettingsLabel = wx.StaticText(self, wx.ID_ANY, u"MIDI - Settings", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.midiSettingsLabel.Wrap(-1)
+        self.midiSettingsLabel.SetFont(
+            wx.Font(13, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Lucida Grande"))
+        self.midiSettingsLabel.SetForegroundColour(wx.Colour(17, 133, 49))
+        verticalBoxes.Add(self.midiSettingsLabel, 0, wx.ALL, 5)
 
         flexGridMidiSettings = wx.FlexGridSizer(0, 2, 0, 50)
-        flexGridMidiSettings.AddGrowableCol(1)
+        flexGridMidiSettings.AddGrowableCol(0)
         flexGridMidiSettings.SetFlexibleDirection(wx.BOTH)
         flexGridMidiSettings.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
         self.midiCableName = wx.StaticText(self, wx.ID_ANY, u"Name of the virtual MIDI cable:", wx.DefaultPosition,
                                            wx.DefaultSize, 0)
         self.midiCableName.Wrap(-1)
-
         flexGridMidiSettings.Add(self.midiCableName, 0, wx.ALL, 5)
 
-        self.midiCableNameInput = wx.TextCtrl(self, wx.ID_ANY, u"my_midi_cable", wx.DefaultPosition, wx.Size(140, -1),
-                                              wx.HSCROLL | wx.TE_RIGHT)
+        self.midiCableNameInput = wx.TextCtrl(self, wx.ID_ANY, u"my_midi_cable", wx.DefaultPosition, wx.Size(136, -1),
+                                              wx.TE_RIGHT)
         flexGridMidiSettings.Add(self.midiCableNameInput, 0, wx.ALL, 5)
-
         verticalBoxes.Add(flexGridMidiSettings, 0, wx.EXPAND | wx.ALL, 5)
 
-        flexGridCreateCable = wx.FlexGridSizer( 0, 2, 0, 0 )
-        flexGridCreateCable.AddGrowableCol( 0 )
-        flexGridCreateCable.AddGrowableCol( 1 )
-        flexGridCreateCable.SetFlexibleDirection( wx.BOTH )
-        flexGridCreateCable.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+        flexGridCreateCable = wx.FlexGridSizer(0, 2, 0, 0)
+        flexGridCreateCable.AddGrowableCol(0)
+        flexGridCreateCable.AddGrowableCol(1)
+        flexGridCreateCable.SetFlexibleDirection(wx.BOTH)
+        flexGridCreateCable.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
-        self.createMidiCableLabel = wx.StaticText( self, wx.ID_ANY,
-                                                   u"Create virtual MIDI cable (using mido library):",
-                                                   wx.DefaultPosition, wx.DefaultSize, 0 )
-        self.createMidiCableLabel.Wrap( -1 )
+        self.createMidiCableLabel = wx.StaticText(self, wx.ID_ANY, u"Create virtual MIDI cable (using mido library):",
+                                                  wx.DefaultPosition, wx.DefaultSize, 0)
+        self.createMidiCableLabel.Wrap(-1)
 
-        flexGridCreateCable.Add( self.createMidiCableLabel, 0, wx.ALL|wx.EXPAND, 5 )
+        flexGridCreateCable.Add(self.createMidiCableLabel, 0, wx.ALL | wx.EXPAND, 5)
 
-        self.createMidiCableCheckbox = wx.CheckBox( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.createMidiCableCheckbox = wx.CheckBox(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                                   0)
         self.createMidiCableCheckbox.SetValue(True)
-        flexGridCreateCable.Add( self.createMidiCableCheckbox, 0, wx.ALL, 5 )
+        flexGridCreateCable.Add(self.createMidiCableCheckbox, 0, wx.ALL, 5)
 
+        verticalBoxes.Add(flexGridCreateCable, 0, wx.EXPAND | wx.ALL, 5)
 
-        verticalBoxes.Add( flexGridCreateCable, 0, wx.EXPAND|wx.ALL, 5 )
-
-        self.setSettingsButton = wx.Button( self, wx.ID_ANY, u"Done", wx.DefaultPosition, wx.DefaultSize, 0 )
-        verticalBoxes.Add( self.setSettingsButton, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
+        self.setSettingsButton = wx.Button(self, wx.ID_ANY, u"Done", wx.DefaultPosition, wx.DefaultSize, 0)
+        verticalBoxes.Add(self.setSettingsButton, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.ALL, 5)
 
         self.SetSizer(verticalBoxes)
         self.Centre()
