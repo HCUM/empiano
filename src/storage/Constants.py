e9511954aae4802378bd0e1d04718ff3becfb9e6
ru59tew <annika.kilian@campus.lmu.de>
2020-09-07 11:14:07 +0200

    small documentation update

--- src/storage/Constants.py
+++ src/storage/Constants.py
@@ -7,7 +7,7 @@ upperBoundCutOutFreq = 100.0
 lowBandStopFreq      = 49
 highBandStopFreq     = 51
 # for feature calculation:
-windowSize           = 0.15 #in s -> 150ms = 0.15s -> our sampling rate = 500Hz-> one sample every 0.002s
+windowSize           = 0.15  #in s -> 150ms = 0.15s
 samplesPerWindow     = int(windowSize / (1. / samplingRate))
 windowShift          = int(samplesPerWindow / 2)
 dataSampleCorrection = 0.000001
